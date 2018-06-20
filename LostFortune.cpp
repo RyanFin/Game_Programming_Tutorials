ed038627b3865ae57d6da5c9a88e7f9608ac5fa0
RyanFin <21296921@student.uwl.ac.uk>
2018-06-20 15:07:03 +0100

    Rewrite Story

--- LostFortune.cpp
+++ LostFortune.cpp
@@ -32,20 +32,18 @@ int main(){
 	
 	cout << "Enter your last name: " << endl;
 	cin >> leader;
+	
+	// Tell the story
 		
-	cout << "a large group of " << adventurers << "set out on a quest -- in search of the lost treasure of Ancient Dwarves." << 
+	cout << "A large group of " << adventurers << " adventurers set out on a quest -- in search of the lost treasure of Ancient Dwarves." << 
 	"The group was led by a legendary rouge, who comes by the name of " << leader <<"." << endl;
+	
 	cout << "\nAlong the way, a band of maurauding ogres attack the party. All fought valiantly under the command of " << 
-	leader << " and the ogres were swiftly defeated, but a cost of " << killed << " lives who perished in the battle. Leaving just " << 
-	(adventurers - killed) << " remaining in the group." <<endl;
+	leader << ", and the ogres were swiftly defeated, but a cost of " << killed << " lives who perished in the battle. Leaving just " << 
+	survivors << " remaining in the group." <<endl;
 	
-	cout << "\nThe party was about to give up all hope. But whilst leaving the deceased to rest, they stumbled upon the buried fortune" <<
-	"So the adventurers split 900 gold pieces." << leader << " held on to the extra four pieces to keep things fair of course." << endl;
-
-	
-
-	
-
+	cout << "\nThe party was about to give up all hope. But whilst leaving the deceased to rest, they stumbled upon the buried fortune." <<
+	" So the adventurers split "<< GOLD_PIECES << " gold pieces." << leader << " held on to the extra "<< (GOLD_PIECES % survivors) << " pieces to keep things fair of course.\n" << endl;
 	
 	system("pause");
 	return 0;
