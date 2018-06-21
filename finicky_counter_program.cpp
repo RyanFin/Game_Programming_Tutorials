3aa8c6edd78bd6f62b7069534815b3cf6d70d228
RyanFin <21296921@student.uwl.ac.uk>
2018-06-21 17:59:20 +0100

    Add Finicky Counter Program

---
+++ finicky_counter_program.cpp
@@ -0,0 +1,31 @@
+// Finicky Counter Program
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	
+	int count = 0;
+	
+	while(true){
+		
+		// increment count
+		count += 1;
+		
+		// exit condition (hence not an infinite loop)
+		if(count > 10){
+			break;
+		}
+		
+		// skip the number 5 
+		if (count == 5){
+			continue;
+		}
+		
+		cout << count << endl;
+	
+	}
+	return 0;
+}
---
+++ infinite_loop.cpp
@@ -0,0 +1,16 @@
+// Infinite Loop
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a = 10;
+	
+	while(a == 10){
+		cout << "I will always Run :)" << endl;
+	}
+	
+	system("pause");
+	
+	return 0;
+}
