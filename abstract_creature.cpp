dd27bdd3cc7fb8cc1cd9e34ff3d6d5a3598dcc18
RyanFin <21296921@student.uwl.ac.uk>
2018-07-07 22:14:01 +0100

    Add Abstract Creature Program

--- abstract_creature.cpp
+++ abstract_creature.cpp
@@ -15,17 +15,30 @@ class Creature{
 		virtual void Greet() const = 0;
 		
 		virtual void displayHealth() const{
-			
+			cout << "Health: " << m_health << endl;
 		}
 		
 	protected:
 		int m_health;
-	
+};
+
+class Orc : public Creature{
+	public:
+		// constructor
+		Orc(int health = 120): Creature(health){
+			
+		}
+		
+		virtual void Greet() const{
+			cout << "The orc grunts hello.\n";
+		}
 };
 
 int main(){
 	
-	
+	Creature* pCreature = new Orc();
+	pCreature->Greet();
+	pCreature->displayHealth();
 	
 	return 0;
 }
